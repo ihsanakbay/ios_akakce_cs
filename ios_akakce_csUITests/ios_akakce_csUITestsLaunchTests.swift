7ed338343bf88e5c4e692aed00a4ed6bfa1a6f03
ihsanakbay <ihsanakbay0@gmail.com>
2025-03-11 13:31:11 +0300

    Initial Commit

---
+++ ios_akakce_csUITests/ios_akakce_csUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_akakce_csUITestsLaunchTests.swift
+//  ios_akakce_csUITests
+//
+//  Created by İhsan Akbay on 11.03.2025.
+//
+
+import XCTest
+
+final class ios_akakce_csUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
